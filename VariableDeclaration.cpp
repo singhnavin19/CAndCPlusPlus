7ff9de1d4357de3b0028a505a6d57ae126f79d27
singhnavin19 <singhnavin19290@gmail.com>
2023-05-23 21:35:30 +0530

    Create VariableDeclaration.cpp
    first commit

---
+++ VariableDeclaration.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    //dataType varibaleName
+    int age;
+    cout<<"initial age ="<< age;
+    
+    age=23;
+    cout<<"\nVarun age= "<<age;
+
+    age=21;
+    cout<<"\nvedika age="<<age;
+    return 0;
+}
